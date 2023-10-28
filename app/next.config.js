1a776c23ed44e3e7645fb5641d4a0990a801d1a4
Vahor <nathan.d0601@gmail.com>
2023-10-28 17:34:29 +0200

    fix Dockerfile standalone path

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
   output: 'standalone',
-  transpilePackages: ['@pedaki/api']
+
+  transpilePackages: ['@pedaki/api'],
+
+  images: {
+    domains: ["static.pedaki.fr"],
+  },
 };
 
 module.exports = nextConfig;
