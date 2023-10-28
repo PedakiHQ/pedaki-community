97fde4ad8c2df80809572bf72b82350cb7ca8851
Vahor <nathan.d0601@gmail.com>
2023-10-28 18:00:19 +0200

    fix pass $IMAGE_NAME in env variable

--- scripts/release-ghcr.sh
+++ scripts/release-ghcr.sh
@@ -1,5 +1,10 @@
 VERSION=$1
 
+if [ -z "$IMAGE_NAME" ]; then
+  echo "IMAGE_NAME not set"
+  exit 1
+fi
+
 if [ -z "$VERSION" ]; then
   echo "No version specified, using package.json version"
   VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d ' ')
@@ -7,8 +12,8 @@ fi
 
 echo "Version: ${VERSION}"
 
-docker build . --tag ghcr.io/pedakihq/pedaki:latest -f ./app/Dockerfile
+docker build . --tag $IMAGE_NAME:latest -f ./app/Dockerfile
 
-docker push ghcr.io/pedakihq/pedaki:latest
-docker tag ghcr.io/pedakihq/pedaki:latest ghcr.io/pedakihq/pedaki:${VERSION}
-docker push ghcr.io/pedakihq/pedaki:${VERSION}
+docker push $IMAGE_NAME:latest
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:${VERSION}
+docker push $IMAGE_NAME:${VERSION}
