9ac89062e0d614e802ad56dd892e540b28fabb09
Vahor <nathan.d0601@gmail.com>
2023-10-29 23:03:46 +0100

    root package.json not updated by changesets

--- scripts/release-ghcr.sh
+++ scripts/release-ghcr.sh
@@ -7,7 +7,7 @@ fi
 
 if [ -z "$VERSION" ]; then
   echo "No version specified, using package.json version"
-  VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d ' ')
+  VERSION=$(cat app/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d ' ')
 fi
 
 echo "Version: ${VERSION}"
