1dfc773956843ab0c170e1f1f63fb55a958df270
Vahor <nathan.d0601@gmail.com>
2023-11-11 11:46:19 +0100

    add provider

--- app/src/app/[locale]/providers.tsx
+++ app/src/app/[locale]/providers.tsx
@@ -2,7 +2,9 @@
 
 import { I18nProviderClient } from '~/locales/client';
 import { TrpcProvider } from '~/server/provider.tsx';
+import { SessionProvider } from 'next-auth/react';
 import React from 'react';
+import { Toaster } from 'sonner';
 
 interface Props {
   children: (React.ReactElement | React.ReactNode) | (React.ReactElement | React.ReactNode)[];
@@ -12,9 +14,12 @@ interface Props {
 export const Providers = ({ children, locale }: Props) => {
   return (
     <>
-      <TrpcProvider>
-        <I18nProviderClient locale={locale}>{children}</I18nProviderClient>
-      </TrpcProvider>
+      <Toaster />
+      <SessionProvider>
+        <TrpcProvider>
+          <I18nProviderClient locale={locale}>{children}</I18nProviderClient>
+        </TrpcProvider>
+      </SessionProvider>
     </>
   );
 };
