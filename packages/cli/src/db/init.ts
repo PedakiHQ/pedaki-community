15fb200be1c352e86131656a8f4238745ca0f485
Vahor <nathan.d0601@gmail.com>
2023-11-19 19:37:17 +0100

    fix: docker build

--- packages/cli/src/db/init.ts
+++ packages/cli/src/db/init.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@pedaki/db';
 import { authService } from '@pedaki/services/auth/auth.service.js';
 import { mailService } from '@pedaki/services/mail/mail.service.js';
-import { CHECK, CROSS, DOLLAR, handleBaseFlags, label } from '~/help.ts';
+import { CHECK, CROSS, DOLLAR, handleBaseFlags, IS_CI, label } from '~/help.ts';
 import type { Command } from '~/types.ts';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
@@ -154,6 +154,11 @@ class DbInitCommand implements Command {
   }
 
   async confirmCreation() {
+    if (IS_CI) {
+      console.log(CHECK + ' Skipping confirmation in CI');
+      return true;
+    }
+
     const question = {
       type: 'confirm',
       name: 'confirm',
