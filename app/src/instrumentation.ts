2baf97806e1fd1a2900d4f179bc64a253a60457e
Vahor <me@vahor.fr>
2023-11-27 21:06:36 +0100

    fix: typo

--- app/src/instrumentation.ts
+++ app/src/instrumentation.ts
@@ -1,1 +1,7 @@
-export function register() {}
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { logger } = await import('@pedaki/logger');
+
+    logger.info(`Starting pedaki app...`);
+  }
+}
