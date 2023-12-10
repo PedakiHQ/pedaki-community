4645c0a8f3c26d5c3991b7346970abe0d94002a6
Vahor <me@vahor.fr>
2023-12-10 22:58:59 +0100

    feat: add prisma decryption-keys

--- packages/db/src/env.ts
+++ packages/db/src/env.ts
@@ -7,6 +7,7 @@ export const env = createEnv({
 
     DATABASE_URL: z.string().min(1),
     PRISMA_ENCRYPTION_KEY: z.string().min(1),
+    PRISMA_DECRYPTION_KEYS: z.array(z.string()),
   },
   runtimeEnv: process.env,
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
