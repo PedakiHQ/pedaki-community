478e8436295aded7f33437814d471de1c292f87c
Vahor <me@vahor.fr>
2023-12-11 18:57:25 +0100

    feat: add error stack

--- packages/api/src/router/middleware/telemetry.middleware.ts
+++ packages/api/src/router/middleware/telemetry.middleware.ts
@@ -1,4 +1,5 @@
 import { trace } from '@opentelemetry/api';
+import { getHTTPStatusCodeFromError } from '@trpc/server/http';
 import { t } from '~api/router/init.ts';
 import { flatten } from 'flat';
 
@@ -6,12 +7,22 @@ export const withTelemetry = t.middleware(async ({ rawInput, path, type, next })
   const tracer = trace.getTracer('trpc');
   return tracer.startActiveSpan(`${path} - ${type}`, async span => {
     const result = await next();
+
+    let statusCode = 200;
+    let stack = null;
+    if (!result.ok) {
+      statusCode = getHTTPStatusCodeFromError(result.error);
+      stack = result.error.stack ?? null;
+    }
+
     span.setAttributes(
       flatten({
         input: rawInput,
         path: path,
         type: type,
         ok: result.ok,
+        stack: stack,
+        statusCode,
       }),
     );
     span.end();
