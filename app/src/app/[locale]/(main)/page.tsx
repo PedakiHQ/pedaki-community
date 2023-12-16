faacbd45df853e0d1256e19a6dfbc5ebc7480c78
AvaN0x <clement.ricatte57@gmail.com>
2023-12-16 19:56:59 +0100

    add missing setStaticParamsLocale

--- app/src/app/[locale]/(main)/page.tsx
+++ app/src/app/[locale]/(main)/page.tsx
@@ -4,9 +4,11 @@ import LocaleButton from '~/components/LocaleButton.tsx';
 import TestAPI from '~/components/TestAPI.tsx';
 import { env } from '~/env.ts';
 import { getI18n } from '~/locales/server.ts';
+import type { LocaleCode } from '~/locales/server.ts';
 import { setStaticParamsLocale } from '~/locales/utils';
 
-export const generateMetadata = async () => {
+export const generateMetadata = async ({ params }: { params: { locale: LocaleCode } }) => {
+  setStaticParamsLocale(params.locale);
   const t = await getI18n();
 
   return {
