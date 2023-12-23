b8544d785611b98db5ec16e65c0f1f4deb048041
Vahor <me@vahor.fr>
2023-12-23 18:52:26 +0100

    update dependencies

--- app/src/settings/index.ts
+++ app/src/settings/index.ts
@@ -1,9 +1,14 @@
 'use server';
 
+import * as process from 'process';
 import { env } from '~/env.ts';
 import { api } from '~/server/clients/internal.ts';
 
 export const getWorkspaceSettings = async () => {
+  if (process.env.CI) {
+    return {};
+  }
+
   const response = await api.workspace.getSettings.query();
 
   // set default values
