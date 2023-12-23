2f2d6b6b04a64e2904c20bb2730a323a71ed5475
Vahor <me@vahor.fr>
2023-12-23 14:28:06 +0100

    add bg-weak behind banner

--- app/src/components/DemoBanner/client.tsx
+++ app/src/components/DemoBanner/client.tsx
@@ -21,35 +21,39 @@ const DemoBannerComponent = () => {
 
   return (
     <div className="peer" data-visible={demoBannerVisible}>
-      <div className="z-1 fixed inset-x-0 m-1 rounded-sm bg-surface px-4 py-2 text-white">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-1 items-center justify-center gap-2">
-            <IconInfoCircleFill className="hidden h-5 w-5 sm:block" />
-            <p className="text-label-sm font-medium">Version de démonstration</p>
-            <p>∙</p>
-            <p className="hidden text-p-sm lg:block">Certaines fonctionnalités sont désactivées</p>
-            <StyledLink
-              href="#"
-              decoration="underline"
-              variant="white"
-              offset={2}
-              className="text-p-sm font-medium"
-            >
-              En savoir plus
-            </StyledLink>
+      <div className="z-1 fixed inset-x-0 bg-weak">
+        <div className="m-1 rounded-sm bg-surface px-4 py-2 text-white">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-1 items-center justify-center gap-2">
+              <IconInfoCircleFill className="hidden h-5 w-5 sm:block" />
+              <p className="text-label-sm font-medium">Version de démonstration</p>
+              <p>∙</p>
+              <p className="hidden text-p-sm lg:block">
+                Certaines fonctionnalités sont désactivées
+              </p>
+              <StyledLink
+                href="#"
+                decoration="underline"
+                variant="white"
+                offset={2}
+                className="text-p-sm font-medium"
+              >
+                En savoir plus
+              </StyledLink>
+            </div>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button onClick={() => setDemoBannerVisible(false)}>
+                    <IconX className="h-5 w-5" />
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent side="bottom" align="end">
+                  Fermer
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <button onClick={() => setDemoBannerVisible(false)}>
-                  <IconX className="h-5 w-5" />
-                </button>
-              </TooltipTrigger>
-              <TooltipContent side="bottom" align="end">
-                Fermer
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
         </div>
       </div>
     </div>
