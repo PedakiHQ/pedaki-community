0b204103271f7a97d454acf24662bd1f7049d4bb
Vahor <me@vahor.fr>
2023-12-28 11:21:52 +0100

    move metadata to root layout

--- app/src/app/[locale]/layout.tsx
+++ app/src/app/[locale]/layout.tsx
@@ -2,13 +2,11 @@ import './globals.css';
 import '@pedaki/design/tailwind/index.css';
 import { BaseProvider } from '~/app/[locale]/baseProvider.tsx';
 import DemoBanner from '~/components/DemoBanner/wrapper';
-import { FAVICON_URL } from '~/constants.ts';
 import type { LocaleCode } from '~/locales/server';
 import { getStaticParams } from '~/locales/server';
 import { locales } from '~/locales/shared';
 import { fixLocale } from '~/locales/utils';
 import { getWorkspaceSettings } from '~/settings';
-import { fetchSettings } from '~/settings/fetch.ts';
 import { COOKIE_NAME } from '~/store/global/constants.ts';
 import type { GlobalStore } from '~/store/global/global.store.ts';
 import GlobalStoreProvider from '~/store/global/StoreProvider.tsx';
@@ -18,21 +16,14 @@ import { cookies } from 'next/headers';
 import { notFound } from 'next/navigation';
 import React, { Suspense } from 'react';
 
-export const generateMetadata = async ({ params }: { params: { locale: LocaleCode } }) => {
+export const generateMetadata = ({ params }: { params: { locale: LocaleCode } }) => {
   const locale = fixLocale(params.locale);
   setStaticParamsLocale(locale);
 
-  const settings = await fetchSettings();
-
   return {
-    title: {
-      template: `%s - ${settings.name}`,
-      default: settings.name,
-    },
     openGraph: {
       locale: locale,
     },
-    icons: [{ rel: 'icon', type: 'image/png', sizes: '32x32', url: FAVICON_URL }],
   };
 };
 
--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,7 +1,42 @@
-import { fallbackLocale } from '~/locales/shared';
+import { FAVICON_URL, LOGO_URL } from '~/constants.ts';
+import { env } from '~/env.ts';
+import { fallbackLocale, locales } from '~/locales/shared';
+import { fetchSettings } from '~/settings/fetch.ts';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const generateMetadata = async () => {
+  const settings = await fetchSettings();
+
+  return {
+    metadataBase: new URL(`https://${env.NEXT_PUBLIC_PEDAKI_HOSTNAME}`),
+    title: {
+      template: `%s - ${settings.name}`,
+      default: settings.name,
+    },
+    description: 'TODO description',
+    applicationName: settings.name,
+    robots: 'noindex, nofollow',
+    alternates: {
+      canonical: '/',
+      languages: {
+        'x-default': '/',
+        ...locales.reduce(
+          (acc, locale) => {
+            acc[locale] = settings.defaultLanguage === locale ? '/' : `/${locale}`;
+            return acc;
+          },
+          {} as Record<string, string>,
+        ),
+      },
+    },
+    icons: [
+      { rel: 'icon', type: 'image/png', sizes: '32x32', url: FAVICON_URL },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '192x192', url: LOGO_URL },
+    ],
+  };
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   // TODO: not possible atm, but we should change the lang to the current locale
