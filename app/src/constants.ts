9a6a2a7398fd119491ce2f0b74c40c30035859dc
Vahor <me@vahor.fr>
2023-12-28 11:22:09 +0100

    add note

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,4 +1,4 @@
 import { env } from '~/env.ts';
 
-export const FAVICON_URL = `${env.NEXT_PUBLIC_PUBLIC_FILES_HOST}/logo/favicon-32x32.png`;
+export const FAVICON_URL = `${env.NEXT_PUBLIC_PUBLIC_FILES_HOST}/logo/favicon-32x32.png`; // Same as in next.config.js
 export const LOGO_URL = `${env.NEXT_PUBLIC_PUBLIC_FILES_HOST}/logo/logo-192x192.png`;
