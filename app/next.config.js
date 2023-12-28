d7f3151c59f39bd1250eacfeea64e3ebf6711f29
Vahor <me@vahor.fr>
2023-12-28 16:53:46 +0100

    fix config

--- app/next.config.js
+++ app/next.config.js
@@ -39,7 +39,7 @@ const nextConfig = {
 
     rewrites() {
       return [
-          process.env.NEXT_PUBLIC_PUBLIC_FILES_HOST.startsWith('http') && {
+          process.env.NEXT_PUBLIC_PUBLIC_FILES_HOST?.startsWith('http') && {
               source: '/favicon.ico',
               destination: `${process.env.NEXT_PUBLIC_PUBLIC_FILES_HOST}/logo/favicon-32x32.png`, // Same as in src/constants.ts
               locale: false
