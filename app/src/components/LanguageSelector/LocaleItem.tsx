2126cca21a964915874dc8679ce349859a69fce9
AvaN0x <clement.ricatte57@gmail.com>
2023-12-31 15:06:05 +0100

    feat: 🌐 more i18n

--- app/src/components/LanguageSelector/LocaleItem.tsx
+++ app/src/components/LanguageSelector/LocaleItem.tsx
@@ -4,6 +4,7 @@ import { DropdownMenuItem } from '@pedaki/design/ui/dropdown-menu';
 import { LocaleIcon } from '~/components/LanguageSelector/LocaleIcon.tsx';
 import { useChangeLocale, useCurrentLocale } from '~/locales/client';
 import type { LocaleCode } from '~/locales/server';
+import { localesLabels } from '~/locales/shared';
 import React from 'react';
 
 export const LocaleItem = ({ locale }: { locale: LocaleCode }) => {
@@ -11,14 +12,12 @@ export const LocaleItem = ({ locale }: { locale: LocaleCode }) => {
   const isActive = locale === activeLocale;
   const Icon = LocaleIcon[locale];
 
-  // const languageT = useScopedI18n('components.footer.language');
   const changeLocale = useChangeLocale({ preserveSearchParams: true });
 
   return (
     <DropdownMenuItem disabled={isActive} key={locale} onClick={() => changeLocale(locale)}>
       <Icon className="h-4 w-5 rounded-sm" />
-      {/*<DropdownMenuLabel>{languageT(locale)}</DropdownMenuLabel>*/}
-      <span>{locale}</span>
+      <span>{localesLabels[locale]}</span>
     </DropdownMenuItem>
   );
 };
--- app/src/locales/fr/settings/general.ts
+++ app/src/locales/fr/settings/general.ts
@@ -6,10 +6,61 @@ export default {
     general: {
       title: 'Général',
       description: 'Paramètres généraux du workspace.',
+      form: {
+        fields: {
+          name: {
+            label: 'Nom du workspace',
+            description: 'blabla sidebar et onglets',
+          },
+          defaultLanguage: {
+            label: "Langue de l'application",
+            description: 'Blabla uniquement celle de base',
+            placeholder: 'Sélectionnez une langue',
+          },
+        },
+        submit: {
+          label: 'Sauvegarder',
+          loading: {
+            title: 'Sauvegarde en cours',
+            description: ' ',
+          },
+          success: {
+            title: '🎉 Informations mises à jour avec succès',
+            description: ' ',
+          },
+          error: {
+            title: 'Une erreur est survenue lors de la mise à jour des informations',
+            description: ' ',
+          },
+        },
+      },
     },
     appearance: {
       title: 'Apparence',
       description: "Paramètres d'apparence du workspace.",
+      form: {
+        fields: {
+          logo: {
+            label: 'Logo du workspace',
+            description: 'blabla la preview peut changer (taille recommendée {width}x{height}})',
+          },
+        },
+        submit: {
+          label: 'Sauvegarder',
+          loading: {
+            title: 'Sauvegarde du logo en cours',
+            description: ' ',
+          },
+          success: {
+            title: '🎉 Logo mis à jour avec succès',
+            description: ' ',
+          },
+          error: {
+            title: 'Une erreur est survenue lors de la mise à jour du logo',
+            description: ' ',
+          },
+        },
+      },
     },
   },
 } as const;
