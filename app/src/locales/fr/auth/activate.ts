dccc6f4a23b7c1571de5f1b39315afea7a6d291b
AvaN0x <clement.ricatte57@gmail.com>
2023-12-31 15:43:47 +0100

    feat: 🌐 more i18n

---
+++ app/src/locales/fr/auth/activate.ts
@@ -0,0 +1,10 @@
+export default {
+  metadata: {
+    title: 'Activation de compte',
+    description: ' ',
+  },
+  wrapper: {
+    title: 'Bonjour {name} !',
+    description: "Avant d'accéder ay workspace, choisis un mot de passe",
+  },
+} as const;
