fa33bcc8b7e7ac223807663ad33edb7b1f15c6fc
Vahor <me@vahor.fr>
2024-01-06 00:10:24 +0100

    coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from "vite-tsconfig-paths";
 import * as path from "path";
-
 const helpersFolder = path.resolve(__dirname, 'packages/services/src/tests/helpers');
+
 export default defineConfig({
     plugins: [tsconfigPaths()],
     test: {
@@ -10,13 +10,13 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json-summary', 'json'],
+            include: ["packages/**/*.ts"],
         },
         setupFiles: [
             `${helpersFolder}/setup.ts`
         ],
         include: [
             '**/*.test.ts',
-            `!${helpersFolder}`,
         ],
         sequence: {
             hooks: "list"
