ccb17e1941f7e2bccca5b4cb7cf47c15e6166ddc
AvaN0x <clement.ricatte57@gmail.com>
2024-01-14 16:22:33 +0100

    feat: 🏷️ create type for i18n functions

--- app/src/store/tutorial/data/locale.ts
+++ app/src/store/tutorial/data/locale.ts
@@ -1,9 +1,9 @@
 'use client';
 
-import type { useScopedI18n } from '~/locales/client';
+import type { UseScopedI18nType } from '~/locales/client';
 import type { Locale } from 'react-joyride';
 
-export const tutorialLocale = (t: ReturnType<typeof useScopedI18n<'tutorial'>>): Locale => ({
+export const tutorialLocale = (t: UseScopedI18nType<'tutorial'>): Locale => ({
   back: t('main.locales.back'),
   close: t('main.locales.close'),
   last: t('main.locales.last'),
--- app/src/store/tutorial/type.ts
+++ app/src/store/tutorial/type.ts
@@ -1,11 +1,11 @@
-import type { useScopedI18n } from '~/locales/client';
+import type { useScopedI18n, UseScopedI18nType } from '~/locales/client';
 import type { TutorialStore } from '~/store/tutorial/tutorial.store.ts';
 import type { BaseProps, CallBackProps, Step } from 'react-joyride';
 
 export interface Tutorial {
   id: string;
   options?: BaseProps;
-  steps: (t: ReturnType<typeof useScopedI18n<'tutorial'>>) => Step[];
+  steps: (t: UseScopedI18nType<'tutorial'>) => Step[];
   callback?: (
     props: Pick<TutorialStore, 'setTutorial' | 'setStepIndex' | 'setPaused' | 'addCompleted'> & {
       push: (url: string) => void;
