dc2041c8afc1b1dfff646c21251c32c33e39ebf9
AlexPiquard <alex.piquard@hotmail.com>
2024-01-19 15:06:50 +0100

    fix: internal session (#103)

--- app/src/app/api/settings/route.ts
+++ app/src/app/api/settings/route.ts
@@ -1,6 +1,7 @@
 import { cache } from '@pedaki/common/cache';
 import { logger } from '@pedaki/logger';
 import { CACHE_KEY } from '~/settings/constants.ts';
+import { env } from '~api/env.ts';
 import { createContext } from '~api/router/context.ts';
 import { appRouter } from '~api/router/router.ts';
 import type { NextRequest } from 'next/server';
@@ -8,6 +9,7 @@ import type { NextRequest } from 'next/server';
 export async function GET(req: NextRequest) {
   const settings = await cache(async () => {
     logger.info('Cache miss for GET /api/settings');
+    req.headers.set('x-pedaki-secret', env.API_INTERNAL_SECRET);
     const ctx = await createContext({ req });
     const caller = appRouter.createCaller(ctx);
     return await caller.settings.getSettings();
--- packages/api/src/router/middleware/session.middleware.ts
+++ packages/api/src/router/middleware/session.middleware.ts
@@ -3,24 +3,43 @@ import type { Context } from '~api/router/context.ts';
 import { t } from '~api/router/init.ts';
 
 // infers the `session` as non-nullable
-const ctxWithUser = (ctx: Context) => {
+const ctxWithUser = (session: Context['session']) => {
   return {
     ctx: {
       session: {
-        ...ctx.session,
-        user: ctx.session!.user,
+        ...session,
+        user: session!.user,
       },
     },
   };
 };
 
+const getInternalSession = (ctx: Context): Context['session'] => {
+  const secret = ctx.headers.get('x-pedaki-secret'); // TODO const secret name
+  if (!secret) return null;
+  return {
+    user: {
+      image: '',
+      name: 'internal',
+      email: 'developers@pedaki.fr',
+      id: '0',
+      emailVerified: true,
+    },
+    expires: new Date().toString(), // TODO ajouter quelque chose ? quelques minutes jsp
+  };
+};
+
 export const isLogged = t.middleware(({ ctx, next }) => {
-  if (!ctx.session?.user) {
+  const internalSession = getInternalSession(ctx);
+
+  if (!internalSession && !ctx.session?.user) {
     throw new TRPCError({
       code: 'UNAUTHORIZED',
       message: 'AUTHENTICATION_REQUIRED',
     });
   }
 
-  return next(ctxWithUser(ctx));
+  const session = internalSession ?? ctx.session;
+
+  return next(ctxWithUser(session));
 });
