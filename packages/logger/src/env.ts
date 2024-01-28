78d716d085089b31bc31d8c77ad8bf76c67e4401
Vahor <me@vahor.fr>
2024-01-28 22:59:21 +0100

    fix: parse array

--- packages/logger/src/env.ts
+++ packages/logger/src/env.ts
@@ -8,7 +8,15 @@ export const env = createEnv({
     LOGGER_NAMESPACE: z.string().default('community'),
     LOGGER_SERVICE_NAME: z.string().default('pedaki'),
 
-    TRANSPORTERS: z.enum(['console', 'other', 'otlp', 'file']).array().default(['console']),
+    TRANSPORTERS: z
+      .string()
+      .transform(value => {
+        if (!value) {
+          return [];
+        }
+        return value.split(',');
+      })
+      .default('console'),
 
     OTLP_ENDPOINT: z.string().optional(),
     OTLP_HEADERS: z
