0f80eadd54d76b8a3230daf4f1cc84af123d9ca3
AvaN0x <clement.ricatte57@gmail.com>
2024-02-04 15:42:24 +0100

    feat: ✨ api utils tests

---
+++ packages/api/src/router/utils.test.ts
@@ -0,0 +1,99 @@
+import { filtersArrayToPrismaWhere, orderByArrayToPrismaOrderBy } from '~api/router/utils';
+import { describe, expect, test } from 'vitest';
+
+describe('utils', () => {
+  describe('filtersArrayToPrismaWhere', () => {
+    test('single operator - eq', () => {
+      const filters = [{ field: 'id', operator: 'eq', value: 1 }];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ id: { equals: 1 } });
+    });
+    test('single operator - like', () => {
+      const filters = [{ field: 'id', operator: 'like', value: 1 }];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ id: { contains: 1 } });
+    });
+
+    test('single negative operator - neq', () => {
+      const filters = [{ field: 'id', operator: 'neq', value: 1 }];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ id: { not: { equals: 1 } } });
+    });
+    test('single negative operator - nlike', () => {
+      const filters = [{ field: 'id', operator: 'nlike', value: 1 }];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ id: { not: { contains: 1 } } });
+    });
+
+    test('single nested field', () => {
+      const filters = [{ field: 'level.id', operator: 'neq', value: 1 }];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ level: { id: { not: { equals: 1 } } } });
+    });
+
+    test('no filters', () => {
+      const result = filtersArrayToPrismaWhere([]);
+      expect(result).toEqual({});
+    });
+
+    test('multiple filters on different field', () => {
+      const filters = [
+        { field: 'id', operator: 'eq', value: 1 },
+        { field: 'level.id', operator: 'neq', value: 1 },
+      ];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ id: { equals: 1 }, level: { id: { not: { equals: 1 } } } });
+    });
+
+    test('multiple of the same field', () => {
+      const filters = [
+        { field: 'id', operator: 'eq', value: 1 },
+        { field: 'id', operator: 'neq', value: 2 },
+      ];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ id: { equals: 1, not: { equals: 2 } } });
+    });
+
+    test('multiple of the same field and operator', () => {
+      const filters = [
+        { field: 'id', operator: 'eq', value: 1 },
+        { field: 'id', operator: 'eq', value: 2 },
+      ];
+      const result = filtersArrayToPrismaWhere(filters);
+      expect(result).toEqual({ id: { equals: 2 } });
+    });
+  });
+
+  describe('orderByArrayToPrismaOrderBy', () => {
+    test('single field', () => {
+      const result = orderByArrayToPrismaOrderBy([['id', 'asc']]);
+      expect(result).toEqual({ id: 'asc' });
+    });
+
+    test('single nested field', () => {
+      const result = orderByArrayToPrismaOrderBy([['level.id', 'asc']]);
+      expect(result).toEqual({ level: { id: 'asc' } });
+    });
+
+    test('multiple fields', () => {
+      const result = orderByArrayToPrismaOrderBy([
+        ['id', 'asc'],
+        ['level.id', 'desc'],
+      ]);
+      expect(result).toEqual({ id: 'asc', level: { id: 'desc' } });
+    });
+
+    test('multiple of the same field', () => {
+      const result = orderByArrayToPrismaOrderBy([
+        ['id', 'asc'],
+        ['id', 'desc'],
+      ]);
+      expect(result).toEqual({ id: 'desc' });
+    });
+
+    test('empty orderBy', () => {
+      const result = orderByArrayToPrismaOrderBy([]);
+      expect(result).toEqual({});
+    });
+  });
+});
