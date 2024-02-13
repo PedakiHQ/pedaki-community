ee96eac4d2b7a9a015a0336d2424fcdf7478ad20
Vahor <me@vahor.fr>
2024-02-13 13:20:11 +0100

    messages schema tutorial

--- app/src/locales/fr/tutorial/schema-student.ts
+++ app/src/locales/fr/tutorial/schema-student.ts
@@ -8,8 +8,20 @@ export default {
     navigate: {
       content: "Accédez à la page de gestion des Schéma d'élèves",
     },
-    mainContent: {
-      content: 'todo',
+    baseInfo: {
+      content:
+        "Dans cette section vous trouverez les informations de base concernant le schéma d'élèves. Ici il n'y a pas de configuration à faire.",
+    },
+    properties: {
+      content:
+        'Dans cette section vous pouvez ajouter, modifier ou supprimer des propriétés pour les élèves. Celà peut correspondre à une moyenne dans une matière, appartenance à un groupe, etc.',
+    },
+    propertiesButton: {
+      content: 'Cliquez ici pour ajouter une propriété.',
+    },
+    propertiesForm: {
+      content:
+        "C'est ici que vous renseignez le nom, et le type de la propriété que vous souhaitez ajouter. Pour le moment seul les notes sont disponibles.",
     },
   },
 } as const;
