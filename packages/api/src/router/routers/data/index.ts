78d381bac2eea2e5f60a33c4ee6ec871a680478c
Vahor <me@vahor.fr>
2024-02-14 09:49:10 +0100

    reset other tables

--- packages/api/src/router/routers/data/index.ts
+++ packages/api/src/router/routers/data/index.ts
@@ -5,12 +5,18 @@ import { z } from 'zod';
 export const dataRouter = router({
   // TODO: add permissions check
   reset: privateProcedure
-    .input(z.enum(['student', 'import']).array())
+    .input(z.enum(['student', 'import', 'class', 'property']).array())
     .mutation(async ({ input }) => {
       for (const type of input) {
         switch (type) {
+          case 'property':
+            await prisma.property.deleteMany({});
+            break;
           case 'student':
             await prisma.student.deleteMany({});
+            break;
+          case 'class':
+            await prisma.class.deleteMany({});
             break;
           case 'import':
             await prisma.import.deleteMany({});
