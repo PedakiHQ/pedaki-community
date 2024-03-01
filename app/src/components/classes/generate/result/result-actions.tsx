dab685d06295de7d852d5d42983064ddb2cfe08e
Vahor <me@vahor.fr>
2024-03-01 19:05:22 +0100

    fix import

--- app/src/components/classes/generate/result/result-actions.tsx
+++ app/src/components/classes/generate/result/result-actions.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { Button } from '@pedaki/design/ui/button';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { useCallback } from 'react';
 import { api } from '~/server/clients/client.ts';
 
 
 const ResultActions = () => {
   const router = useRouter()
-  
+
   const confirm = useCallback(() => {
     api.classes.create()
     router.push("/classes/edit/")
